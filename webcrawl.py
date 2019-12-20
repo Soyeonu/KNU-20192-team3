4acf8ed1df588a86121beb2202e7c44f7500dfdd
kafuowi <48575859+kafuowi@users.noreply.github.com>
2019-12-20 22:08:51 +0900

    Update webcrawl.py
    웹크롤러 기능 추가 밑, 학기 변화에 대응 가능하게 함.

--- webcrawl.py
+++ webcrawl.py
@@ -5,23 +5,28 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 import time
-import scrapy
-import requests
-from bs4 import BeautifulSoup
-from selenium.webdriver.support.wait import WebDriverWait
 
 
 #에브리 타임 시간표 자료를 크롤링 하는 코드
 url = "https://knu.everytime.kr/login?redirect=%2Ftimetable"
 
 
-driver = webdriver.Firefox(executable_path="D:/cloud/google/pressx/webdriver/geckodriver.exe")#드라이버 주소
-delay = 10  # seconds
+driver = webdriver.Firefox(executable_path="C:/Users/사용자/Google 드라이브/webdriver/geckodriver.exe")#드라이버 주소
+delay = 3  # seconds
 driver.get(url)
-driver.find_element_by_name('userid').send_keys('Id') #에브리타임 id
+driver.find_element_by_name('userid').send_keys('Id') # 에브리타임 id
 driver.find_element_by_name('password').send_keys('userpw') # 에브리타임 비밀번호
 driver.find_element_by_name('password').send_keys(Keys.ENTER)
 time.sleep(3)
+
+
+semester = input("input semester(형식:xxxx년 x학기): ")
+drop = driver.find_element_by_id("semesters")
+
+drop.find_element_by_xpath('//option[contains(text(), "' + semester + '")]').click()
+
+time.sleep(3)
+
 ul_elem = driver.find_element_by_class_name("floating")
 
 li_elem = ul_elem.find_element_by_tag_name("li")
@@ -32,19 +37,19 @@ filters = subjects.find_element_by_class_name("filter")
 item = filters.find_element_by_class_name("item")
 
 driver.execute_script("arguments[0].click();", item)
-f = open("새파일.txt", 'w')
+fname = input("파일이름을 입력하세요:")
+f = open(fname+".txt", 'w')
 time.sleep(2)
 subjects = driver.find_element_by_id("subjectCategoryFilter")
 filters = subjects.find_element_by_class_name("filter")
 category = filters.find_element_by_class_name("category")
 li_elem = category.find_elements_by_tag_name("li")
-driver.execute_script("arguments[0].click();", li_elem[179])# 컴퓨터학부
+majorname=input("찾아볼 전공을 입력하세요:")
+major = category.find_element_by_xpath('//li[contains(text(), "'+majorname+'")]')
+driver.execute_script("arguments[0].click();", major)# 컴퓨터학부
 time.sleep(2)
 list = driver.find_element_by_class_name("list")
-f.write(list.text)
-print(list.text)
-driver.execute_script("arguments[0].click();", li_elem[180])#글로벌 sw
-time.sleep(2)
-list = driver.find_element_by_class_name("list")
-f.write(list.text)
-print(list.text)
+body = list.find_element_by_tag_name("tbody")
+
+f.write(body.text)
+print(body.text)
